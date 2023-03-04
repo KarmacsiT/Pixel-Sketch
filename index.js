9f95895e1b0945cd4b34fbee3835d230af7407ad
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2023-03-04 21:43:38 +0100

    Add 16x16 grid

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+window.onload = drawGrid();
 
+function drawGrid() {
+	const container = document.querySelector(".grid");
+
+	for (let i = 0; i < 256; i++) {
+		let block = document.createElement("div");
+
+		block.classList.add("block");
+		container.appendChild(block);
+	}
+}
