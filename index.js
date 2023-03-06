70ca37235714e90776b654b27401d49c35e490c7
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2023-03-06 11:11:09 +0100

    Add random color change to blocks on mouse enter

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function drawGrid() {
 		let block = document.createElement("div");
 
 		block.classList.add("block");
+		block.addEventListener("mouseover", () => {
+			setRandomCellColor(block);
+		});
+
 		container.appendChild(block);
 	}
+}
+
+function setRandomCellColor(cell) {
+	cell.style.backgroundColor = generateRandomColor();
+}
+
+function generateRandomColor() {
+	return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
