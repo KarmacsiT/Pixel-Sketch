714618a17c0f7d3b66ead7e0ae7395d82b54a746
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2023-03-08 12:57:42 +0100

    Add resize grid function and page styles

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 let isMouseDown;
 let userColor;
 
-window.onload = drawGrid();
+window.onload = initializePage();
 
 document.addEventListener("coloris:pick", (userColorPick) => {
 	userColor = userColorPick.detail.color;
 });
+
+function initializePage() {
+	let resizeButton =
+		document.querySelector(".button-wrapper").firstElementChild;
+	resizeButton.onclick = function () {
+		getNewGridSize();
+	};
+
+	drawGrid();
+}
+
+function getNewGridSize() {
+	let gridSize = prompt("Please enter your desired grid size!");
+	const container = document.querySelector(".grid");
+	container.replaceChildren();
+	drawGrid(gridSize);
+}
 
 function drawGrid(gridSize = 16) {
 	const container = document.querySelector(".grid");
@@ -34,6 +51,8 @@ function drawGrid(gridSize = 16) {
 
 		container.appendChild(block);
 	}
+	container.style.gridTemplateColumns = `repeat(${gridSize}, minmax(5px, 60px))`;
+	container.style.gridTemplateRows = `repeat(${gridSize}, minmax(5px, 60px))`;
 }
 
 function checkCellColoringCondition(cell) {
